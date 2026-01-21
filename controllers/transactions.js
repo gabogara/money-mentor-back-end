21419979c550a201d55eb8b0d560964ac66f6894
gabogara <gabo1gara@gmail.com>
2026-01-20 20:36:03 -0800

    feat: Add POST /transactions route

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,16 +1,30 @@
-const express = require('express');
+const express = require("express");
 const verifyToken = require("../middleware/verify-token.js");
 const Transaction = require("../models/transaction.js");
 const router = express.Router();
 
-
 // GET /transactions - list only current user's transactions
 router.get("/", verifyToken, async (req, res) => {
   try {
-    const transactions = await Transaction.find({ userId: req.user._id })
-      .sort({ date: -1, createdAt: -1 });
+    const transactions = await Transaction.find({ userId: req.user._id }).sort({
+      date: -1,
+      createdAt: -1,
+    });
 
     res.status(200).json(transactions);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// POST /transactions - create (assign userId from token)
+router.post("/", verifyToken, async (req, res) => {
+  try {
+    req.body.userId = req.user._id;
+
+    const transaction = await Transaction.create(req.body);
+
+    res.status(201).json(transaction);
   } catch (err) {
     res.status(500).json({ err: err.message });
   }
